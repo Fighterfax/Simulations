9a9a1261afe873d06fcffd3c00037d77591974d5
Fighterfax <dentonwhiteside@amdg.rockhursths.edu>
2020-02-18 08:39:27 -0600

    yessir

--- cards.py
+++ cards.py
@@ -1,16 +1,26 @@
-Python 3.7.2 (v3.7.2:9a3ffc0492, Dec 24 2018, 02:44:43) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-== RESTART: /Users/dentonwhiteside/Documents/APCSP/git/Simulations/cards.py ==
-[]
->>> 
-== RESTART: /Users/dentonwhiteside/Documents/APCSP/git/Simulations/cards.py ==
-3 13
->>> 
-== RESTART: /Users/dentonwhiteside/Documents/APCSP/git/Simulations/cards.py ==
-['A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', 'J♠', 'Q♠', 'K♠', '13♠', 'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', 'J♣', 'Q♣', 'K♣', '13♣', 'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', 'J♥', 'Q♥', 'K♥', '13♥', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', 'J♦', 'Q♦', 'K♦', '13♦']
->>> 
-== RESTART: /Users/dentonwhiteside/Documents/APCSP/git/Simulations/cards.py ==
-['A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠', 'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣', 'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦']
->>> 
+import random
+deck = []
+suits = ["♠","♣","♥","♦"]
+for i in range(4):
+    for j in range(1,14):
+        value = str(j)
+        if (j == 11):
+            value = "J"
+        elif (j == 12):
+            value = "Q"
+        elif (j == 13):
+            value = "K"
+        elif (j == 1):
+            value = "A"
+        deck.append(f"{value}{suits[i]}")
+        
+print(deck)
+
+for i in range(len(deck)-1, 0, -1): 
+
+    j = random.randint(0, i + 1)  
+
+
+    deck[i], deck[j] = deck[j], deck[i]  
+
+print ("The shuffled deck is : " +  str(deck))
