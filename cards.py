3df599d813ec981607b08d1500dbda73efcea457
Fighterfax <dentonwhiteside@amdg.rockhursths.edu>
2020-02-18 08:27:27 -0600

    Cards

---
+++ cards.py
@@ -0,0 +1,16 @@
+Python 3.7.2 (v3.7.2:9a3ffc0492, Dec 24 2018, 02:44:43) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: /Users/dentonwhiteside/Documents/APCSP/git/Simulations/cards.py ==
+[]
+>>> 
+== RESTART: /Users/dentonwhiteside/Documents/APCSP/git/Simulations/cards.py ==
+3 13
+>>> 
+== RESTART: /Users/dentonwhiteside/Documents/APCSP/git/Simulations/cards.py ==
+['A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', 'J♠', 'Q♠', 'K♠', '13♠', 'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', 'J♣', 'Q♣', 'K♣', '13♣', 'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', 'J♥', 'Q♥', 'K♥', '13♥', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', 'J♦', 'Q♦', 'K♦', '13♦']
+>>> 
+== RESTART: /Users/dentonwhiteside/Documents/APCSP/git/Simulations/cards.py ==
+['A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠', 'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣', 'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦']
+>>> 
